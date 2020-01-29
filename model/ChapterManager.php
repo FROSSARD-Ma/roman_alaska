6197dd45b284290ed64d0d43c660f43ee7f8e57b
Marie.FROSSARD <marie@spotsweb.fr>
2020-01-29 10:11:10 +0100

    Travail sur prev - next chapter

--- model/ChapterManager.php
+++ model/ChapterManager.php
@@ -6,7 +6,7 @@ class ChapterManager extends Manager
 {
     //========= CRUD Chapitre ======== */
 
-    /*---  AJOUT Chapitre  ------------------- */
+    /*---  CREAT -------------------------------------------------------- */
     public function addChapter($title, $texte)
     {
         $sql ='INSERT INTO alaska_chapters(title_chapter, content_chapter) 
@@ -19,29 +19,14 @@ class ChapterManager extends Manager
         return $datas;
     }
 
-    /*---  MODIFICATION Chapitre  ------------------- */
-    public function updateChapter()
-    {
-        $sql = ' UPDATE alaska_chapters
-            SET 
-                chapter_date_edit=NOW(),
-                chapter_num=?,
-                chapter_title=?, 
-                chapter_content=?, 
-                chapter_img=?, 
-                chapter_img_alt=?,
-                chapter_statut=?) 
-            VALUES(?, ?, ?, ?, ?, ?)';
+    /*---  READ ---------------------------------------------------------- */
 
-    }
-
-
-    /*---  LISTE de tous les Chapitres  ------- */
+    /*--- LISTE Chapitres -- */
     public function getChapters()
     {
         $sql = 'SELECT *
             FROM alaska_chapters
-            WHERE statut_chapter="published" 
+            WHERE statut_chapter="Publié" 
             ORDER BY num_chapter ASC';
 
         $datas = $this->reqSQL($sql);
@@ -52,7 +37,7 @@ class ChapterManager extends Manager
         return $chapters;
     }
    
-    /*---  DETAILS un Chapitre  ------- */
+    /*--- DETAILS un Chapitre ---- */
     public function getChapter($id)
     {
         $idChapter = (int)$id;
@@ -65,12 +50,12 @@ class ChapterManager extends Manager
         return $chapter;
     }
 
-     /*---  Home 2 Chapitres  ------- */
+     /*--- Home 2 Chapitres --- */
     public function twoChapters($limit = 2)
     {
         $sql = 'SELECT *
         FROM alaska_chapters
-        WHERE statut_chapter ="published" 
+        WHERE statut_chapter ="Publié" 
         ORDER BY num_chapter DESC
         LIMIT 0,'.$limit;
         
@@ -86,7 +71,7 @@ class ChapterManager extends Manager
     {
         $sql ='SELECT *
             FROM alaska_chapters
-            ORDER BY num_chapter ASC';
+            ORDER BY id_chapter ASC';
         $datas = $this->reqSQL($sql);
 
         // Chapters List
@@ -94,14 +79,66 @@ class ChapterManager extends Manager
             $chapter = new \Alaska_Model\Chapter($data);
             $chapters[] = $chapter; // Tableau d'objet
         }
-        // Count Comments
-        foreach ($chapters as $chapter ) {
-            $countManager = new \Alaska_Model\CommentManager;
-            $count[$chapter->getId()] = $countManager->countComments($chapter->getId());
-            
-            //if ($count > 0) echo $count;
-        }
         return $chapters;
+    }
+
+    public function prevChapter($id)
+    {
+        $prevChapter = $id-1;
+        $sql ='SELECT id_chapter
+            FROM alaska_chapters 
+            WHERE id_chapter = ? AND statut_chapter="Publié"';
+        $data = $this->reqSQL($sql, array ($prevChapter), $one = true);
+        if ($data) {
+            $idChapter = implode($data);
+            echo '<a href="index.php?page=chapter/id/'.$idChapter.'" class="button left"><i class="fas fa-arrow-alt-circle-left"></i> Chapitre précedent</a>';
+        }
+    }
+
+    public function nextChapter($num)
+    {
+        $nextChapter = $num+1;
+        $sql ='SELECT id_chapter
+            FROM alaska_chapters 
+            WHERE num_chapter = ? AND statut_chapter="Publié"';
+        $data = $this->reqSQL($sql, array ($nextChapter), $one = true);
+        if ($data) {
+            $idChapter = implode($data);
+            if ($idChapter>=0)
+            {
+                echo '<a href="index.php?page=chapter/id/'.$idChapter.'" class="button right">Chapitre suivant <i class="fas fa-arrow-alt-circle-right"></i></a>';
+            }
+        }
+    }
+
+    /*---  UPDATE -------------------------------------------------------- */
+    public function updateChapter($id)
+    {
+        $idChapter = (int)$id;
+        $sql ='UPDATE alaska_chapters 
+            SET modified_chapter = now(), num_chapter = :num, title_chapter=:title, content_chapter=:texte, img_chapter=:image, imgAlt_chapter=:imgAlt, statut_chapter=:statut 
+            WHERE  id_chapter = :idChapter';
+        $datas = $this->getPDO()->prepare($sql);
+        $datas->bindValue(':num', $_POST['num'], PDO::PARAM_INT); 
+        $datas->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+        $datas->bindValue(':texte', $_POST['texte'], PDO::PARAM_STR);
+        $datas->bindValue(':image', $_POST['image'], PDO::PARAM_STR);
+        $datas->bindValue(':imgAlt', $_POST['imgAlt'], PDO::PARAM_STR);
+        $datas->bindValue(':statut', $_POST['statut'], PDO::PARAM_STR);
+        $datas->bindValue(':idChapter', $idChapter, PDO::PARAM_STR); 
+        $datas->execute();  
+        return $datas;
+    }
+
+    /*---  DELETE -------------------------------------------------------- */
+    public function deleteChapter($id)
+    {
+        $idChapter = (int)$id;
+        $sql ='DELETE FROM alaska_chapters WHERE id_chapter = :idChapter';
+        $data = $this->getPDO()->prepare($sql);
+        $data->bindValue(':idChapter', $idChapter, PDO::PARAM_STR);  
+        $data->execute();
+        return $data;
     }
 
 }
