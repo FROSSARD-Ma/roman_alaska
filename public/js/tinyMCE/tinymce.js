b09779b4ad2b018eb0d4202907d02577d6886ae9
Marie.FROSSARD <marie@spotsweb.fr>
2020-01-31 11:21:36 +0100

    TinyMCE - Langue français
    Paramètrage du menu en français.
    Ajout fichier langue

--- public/js/tinyMCE/tinymce.js
+++ public/js/tinyMCE/tinymce.js
@@ -4,14 +4,12 @@ tinymce.init({
   menubar: false,
   style_formats:
   [
-    { title: 'Headings', items:
-      [
-        { title: 'Titre 1', format: 'h1' },
-        { title: 'Titre 2', format: 'h2' },
-        { title: 'Titre 3', format: 'h3' }
-      ]
-    }
+    { title: 'Titre 1', format: 'h1' },
+    { title: 'Titre 2', format: 'h2' },
+    { title: 'Titre 3', format: 'h3' }
   ],
+  language: 'fr_FR',
+  language_url : 'public/js/tinyMCE/fr_FR.js', // site absolute URL
   toolbar: 'styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | outdent indent',
   browser_spellcheck: true, // correcteur orthographe du navigateur
   spellchecker_languages: 'French=fr_FR',
