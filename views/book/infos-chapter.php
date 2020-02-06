d53528ea394a05c9150f06967f5b4bac3fbe348a
Marie.FROSSARD <marie@spotsweb.fr>
2020-02-06 17:05:38 +0100

    Ajout sécurité risque d'injection
    Ajout htmlspecialchars()

--- views/book/infos-chapter.php
+++ views/book/infos-chapter.php
@@ -7,7 +7,7 @@ $imgAlt = $chapter->getImgAlt();
 ?>
 	<article class="media">
 	    <a href="index.php?page=chapter/id/<?=$id?>"><img src="public/img/chapter/<?=$img?>" alt="<?=$imgAlt?>"></a>
-	    <div class="media-body">
+	    <div class="container">
 	      	<h4 class="mt-0 mb-1"> <?=$chapter->getTitle();?></h4>
 			<p>
 				<?php if ($chapter->getNum()!=0) echo 'Chapitre ' . $chapter->getNum().' - '; ?>
@@ -15,7 +15,7 @@ $imgAlt = $chapter->getImgAlt();
 				<br><?=$chapter->getCountComment();?>
 			</p>
 	      	<p><?=$chapter->getExtract();?></p>
-			<a href="index.php?page=chapter/id/<?=$id?>" class="button right" role="button">Découvrir le chapitre</a>
+			<a href="index.php?page=chapter/id/<?=$id?>" class="button" role="button">Découvrir le chapitre</a>
 	    </div>
 	</article>
 	<hr>
